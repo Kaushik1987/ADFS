45a2f39ef2a1eb3a76250b223bf894e4b77b23fb
kaushik.thanki <kaushik.thanki@clariontechnologies.co.in>
2017-06-20 19:26:38 +0530

    Init checkin

---
+++ ADFS_Login/ADFS_Login/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ADFS_Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            ViewBag.ClaimsIdentity = Thread.CurrentPrincipal.Identity;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ValidateClaim()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ValidateClaim(FormCollection formData)
+        {
+            var claimname = formData["claimname"];
+            var claimvalue = formData["claimvalue"];
+            var userClaims = Thread.CurrentPrincipal.Identity;
+            var claimsIdentity = userClaims as ClaimsIdentity;
+            
+            // Access claim
+            try
+            {
+                if (claimsIdentity != null)
+                {
+                    var isValid = claimsIdentity.Claims.FirstOrDefault(x => x.Type == claimname && x.Value == claimvalue);
+                    ViewBag.Result = isValid != null ? "Claim matched " : "Claim data mismatched ";
+                    ViewBag.isValidClaim = isValid != null;
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.error = ex.Message;
+            }
+            return View(formData);
+        }
+    }
+}
---
+++ ADFS_Login/ADFS_Login/Controllers/StartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADFS_Login.Controllers
+{
+    public class StartController : Controller
+    {
+        // GET: Start
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
